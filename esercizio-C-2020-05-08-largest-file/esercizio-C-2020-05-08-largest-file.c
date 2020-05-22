8395b6a6f5e5c02f1e120d509a9c3506178da85c
utente <utente@debian10.units.it>
2020-05-22 17:20:41 +0200

    DIR access error

--- esercizio-C-2020-05-08-largest-file/esercizio-C-2020-05-08-largest-file.c
+++ esercizio-C-2020-05-08-largest-file/esercizio-C-2020-05-08-largest-file.c
@@ -1,44 +1,78 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <unistd.h>
 #include <string.h>
 
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+#include <errno.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <dirent.h>
 
 // https://www.gnu.org/software/libc/manual/html_node/Simple-Directory-Lister.html
 
-#include <sys/stat.h>
-#include <unistd.h>
-
-//int stat(const char *file_name, struct stat *buf);
-//off_t value = buf->st_size;
-//int value = (int)buf->st_size;	dimensione totale in bytes
-
-int MAX;
+__off_t MAX;
 
 int check=0;
+
+sem_t * semaforo;
+sem_t * semaforo_dir;
+
 
 //struct stat *statbuf;
 
 void check_dir();
 void check_sub_dir();
 
-//statbuf = calloc(1, sizeof(stat));
-char * find_largest_file(char * directory_name, int explore_subdir, int * largest_file_size){
+// restituisce la dimensione del file, -1 in caso di errore
+__off_t get_file_size(char * file_name) {
 
+	struct stat sb;
+	int res;
+
+	res = stat(file_name, &sb);
+
+	if (res == -1) {
+		perror("stat()");
+		return -1;
+	}
+
+	return sb.st_size;
+}
+
+//statbuf = calloc(1, sizeof(stat));
+char * find_largest_file(char * directory_name, int explore_subdir, __off_t * largest_file_size){
 
 	char * name;
 
 	name = malloc(256);
 
 	if(explore_subdir == 0){
+
+		check =0;
 		check_dir(directory_name, name);
 	}
 
 	else{
+		check =0;
+
+		if (sem_wait(semaforo_dir) == -1) {
+				perror("sem_wait");
+				exit(EXIT_FAILURE);
+		}
+
+		check_dir(directory_name, name);
+
+		if (sem_post(semaforo_dir) == -1) {
+				perror("sem_post");
+				exit(EXIT_FAILURE);
+		}
+
 		check_sub_dir(directory_name, name);
 	}
 	return name;
@@ -51,40 +85,53 @@ void check_dir(char * directory_name, char * name){
 	dir_stream_ptr = opendir(directory_name);
 
 	if (dir_stream_ptr == NULL) {
-		printf("impossibile accedere alla DIR %s!\n", directory_name);
+		printf("[check_dir] impossibile accedere alla DIR %s!\n", directory_name);
+		//printf("%s", errno);
 		return;
 		//exit(EXIT_FAILURE);
 	}
+	//printf("accedo alla DIR %s!\n", directory_name);
 
 	struct dirent *ep;
-
-	check =0;
 
 	while ((ep = readdir(dir_stream_ptr)) != NULL) {
 
 			if(ep->d_type == DT_REG){
+				char pathname[256];
 
-				static FILE * pf;
-				pf = fopen(ep->d_name, "r");
+				//strcpy(pathname, "/");
+				strcpy(pathname, directory_name);
+				strcat(pathname, "/");
+				strcat(pathname, ep->d_name);
 
-				if(pf != NULL){
-					fseek(pf, 0, SEEK_END);
+				if(MAX < get_file_size(pathname)){//valuto lunghezza del file(devo però poterci accedere in lettura)
 
-					if(MAX < ftell(pf)){	//valuto lunghezza del file(devo però poterci accedere in lettura)
-						MAX = ftell(pf);
-						memcpy(name, ep->d_name, 256);
+						if (sem_wait(semaforo) == -1) {
+								perror("sem_wait");
+								exit(EXIT_FAILURE);
+						}
+
+						MAX = get_file_size(pathname);
+						memcpy(name, pathname, 256);
+						check++; //valuto se ho potuto accedere ad almeno un file
+
+						if (sem_post(semaforo) == -1) {
+								perror("sem_post");
+								exit(EXIT_FAILURE);
+						}
+
 					}
-					check++; //valuto se ho potuto accedere ad almeno un file
-					fclose(pf);
-				}else{
-					printf("impossibile accedere al FILE %s!\n", ep->d_name);
-				}
-			}
 
-	}
+				}else{
+					//printf("impossibile accedere al FILE %s!\n", ep->d_name);
+				}
+		}
 
 	closedir(dir_stream_ptr);
-}
+	}
+
+
+
 
 void check_sub_dir(char * directory_name, char * name){
 
@@ -92,17 +139,37 @@ void check_sub_dir(char * directory_name, char * name){
 	dir_stream_ptr = opendir(directory_name);
 
 	if (dir_stream_ptr == NULL) {
-		printf("impossibile accedere alla DIR %s!\n", directory_name);
-
-		exit(EXIT_FAILURE);
+		printf("[check_sub_dir] impossibile accedere alla DIR %s!\n", directory_name);
+		return;
+		//exit(EXIT_FAILURE);
 	}
 
 	struct dirent *ep;
+	//char dot[256] = "..";
 
 	while ((ep = readdir(dir_stream_ptr)) != NULL) {
 
-			if(ep->d_type == DT_DIR){
+			if(ep->d_type == DT_DIR && *ep->d_name != '.' && ep->d_name[1] != '.'){
+
+				if (sem_wait(semaforo_dir) == -1) {
+						perror("sem_wait");
+						exit(EXIT_FAILURE);
+				}
+
+				strcat(directory_name, "/");
+				strcat(directory_name, ep->d_name);
+
+				printf("\n%s\n\n", directory_name);
+
 				check_dir(ep->d_name, name);
+
+				if (sem_post(semaforo_dir) == -1) {
+						perror("sem_post");
+						exit(EXIT_FAILURE);
+				}
+
+				check_sub_dir(ep->d_name, name);
+
 			}
 
 	}
@@ -116,6 +183,12 @@ int main(int argc, char *argv[]) {
 // man 3 opendir
 //    DIR *opendir(const char *name);
 //    DIR *fdopendir(int fd);
+	semaforo_dir = malloc(sizeof(sem_t));
+	sem_init(semaforo_dir, 1, 1);
+
+	semaforo = malloc(sizeof(sem_t));
+
+    sem_init(semaforo, 1, 1);
 
 	char * dirName;
 
@@ -130,6 +203,7 @@ int main(int argc, char *argv[]) {
 	}
 
 	struct dirent *ep;
+	check = 0;
 
 	while ((ep = readdir(dir_stream_ptr)) != NULL) {
 
@@ -143,14 +217,15 @@ int main(int argc, char *argv[]) {
 
 		//getcwd(NULL,0) con lo 0 adatta le dimensioni in base alla dimensione della stringa
 
-		printf("Nella cartella: %s\n", getcwd(NULL,0));
+		printf("\nNella cartella: %s\n", getcwd(NULL,0));
 
-		if(check != 0){
+		if(check>0){
 
-		printf("Il file più grande è %s e pesa : %d bytes\n", dirName, MAX);
+		printf("Il file più grande è %s e pesa : %ld bytes\n", dirName, MAX);
 		}
 		else{
 			printf("\nC'è stato un errore nell'accesso ai file, non è possibile valutare la dimensione\n");
+			printf("sarebbe %s e pesa : %ld bytes\n", dirName, MAX);
 		}
 
 		return EXIT_SUCCESS;
